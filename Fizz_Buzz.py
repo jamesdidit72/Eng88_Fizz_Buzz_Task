bdcfc53902c87f89f6ed6fde4207983547aafadf
jamesdidit72 <jamesjbarton72@gmail.com>
2021-05-21 12:55:32 +0100

    comments added to the code

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,14 +1,16 @@
-class Fizzbuzz:
-    def buzz_of_the_fizz(self, required_range):
-        for number in range(required_range + 1):
+class Fizzbuzz:  # initialisation of the class
+    def buzz_of_the_fizz(self, required_range):  # initialisation of the function, self is required.
+        for number in range(required_range + 1):  # looping through the variable + 1
 
-            if number % 3 == 0 and number % 5 == 0:
+            if number % 3 == 0 and number % 5 == 0:  # if the current value of number can be divided by 3 and 5 with no remainder, print
                 print('FizzBuzz')
-            elif number % 3 == 0:
+            elif number % 3 == 0:  # if the current value of number can be divided by 3 with no remainder, print
                 print('Fizz')
-            elif number % 5 == 0:
+            elif number % 5 == 0:  # if the current value of number can be divided by 5 with no remainder, prin
                 print('Buzz')
             else:
-                print(number)
-fizzbuzz_checker = Fizzbuzz()
-fizzbuzz_checker.buzz_of_the_fizz(100)
+                print(number)  # print the value of number
+
+
+fizzbuzz_checker = Fizzbuzz()  # sets the class as a local variable
+fizzbuzz_checker.buzz_of_the_fizz(100) # calls the class and sets the variable to 100
--- README.md
+++ README.md
@@ -22,3 +22,23 @@ class Fizzbuzz:
 fizzbuzz_checker = Fizzbuzz()
 fizzbuzz_checker.buzz_of_the_fizz(100)
 ```
+
+#### second iteration
+```python
+class Fizzbuzz:  # initialisation of the class
+    def buzz_of_the_fizz(self, required_range):  # initialisation of the function, self is required.
+        for number in range(required_range + 1):  # looping through the variable + 1
+
+            if number % 3 == 0 and number % 5 == 0:  # if the current value of number can be divided by 3 and 5 with no remainder, print
+                print('FizzBuzz')
+            elif number % 3 == 0:  # if the current value of number can be divided by 3 with no remainder, print
+                print('Fizz')
+            elif number % 5 == 0:  # if the current value of number can be divided by 5 with no remainder, prin
+                print('Buzz')
+            else:
+                print(number)  # print the value of number
+
+
+fizzbuzz_checker = Fizzbuzz()  # sets the class as a local variable
+fizzbuzz_checker.buzz_of_the_fizz(100) # calls the class and sets the variable to 100
+```
